a81b26dbde96ae1bc64f4e42a66b521b4d6c02fa
Tim Fischer <emailtimfischer@gmail.com>
2018-09-26 17:23:45 -0600

    Add sortAllWords function

--- script.js
+++ script.js
@@ -15,3 +15,27 @@ testNum.then(function (response) {
 }).catch(function (error) {
   console.log(num + ' is less than 10, error!')
 })
+
+var words = ['worms', 'arms', 'dogs']
+
+makeAllCaps = new Promise(function (resolve, reject) {
+  // A mock async action using setTimeout
+  setTimeout(function () { resolve(words); }, 1000);
+})
+  .then(function (words) { return words.map(word => word.toUpperCase()) })
+  .then(function (result) { console.log(result) })
+
+
+
+sortAllWords = new Promise(function (resolve, reject) {
+  if (words.every(word => typeof word === 'string')) {
+    resolve('Success!');
+  }
+  else {
+    reject('Failure!');
+  }
+
+})
+  .then(function (resolve) { console.log('success') })
+  .catch(function (reject) { console.log('No, the array you passed in contained an element that was not a string!') })
+
