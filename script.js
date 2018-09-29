f7c9300dbaff852f3da7e3c2881ced09759766b0
Tim Fischer <emailtimfischer@gmail.com>
2018-09-28 18:35:39 -0600

    Remove setTimeout

--- script.js
+++ script.js
@@ -16,28 +16,26 @@ testNum.then(function (response) {
   console.log(num + ' is less than 10, error!')
 })
 
-
 makeAllCaps = new Promise(function (resolve, reject) {
   let words = ['worms', 'birds', 'dogs']
   if (words.every(word => typeof word === 'string')) {
-    setTimeout(() => resolve(words), 1000);
+    const upperCase = words.map(word => word.toUpperCase())
+    resolve(upperCase);
   }
   else {
     reject('No, the array you passed in contained an element that was not a string!');
   }
 })
 makeAllCaps.then((words) => sortAllWords(words))
-makeAllCaps.then(function (result) { console.log(result) })
-makeAllCaps.catch(function (reject) { console.log(reject) })
+makeAllCaps.then((result) => console.log(result))
+makeAllCaps.catch((reject) => console.log(reject))
 
 
-function sortAllWords(words) {
+sortAllWords = (words) => {
   new Promise(function
     (resolve, reject) {
     resolve(words.sort());
   })
-  .then(function (words) { return words })
-  .catch(function (reject) { console.log('No, the array you passed in contained an element that was not a string!') })
+  .then((words) => words)
+  .catch((reject) => console.log('Items could not be sorted!'))
 }
-
-console.log(makeAllCaps)
